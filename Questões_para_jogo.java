d3fb3e6cf474accd4efd80a25d54fca7f5394f21
Camiilo-Rafa <72233498+Camiilo-Rafa@users.noreply.github.com>
2020-11-03 12:58:08 -0300

    Update Questões_para_jogo.java

--- Questões_para_jogo.java
+++ Questões_para_jogo.java
@@ -16,6 +16,47 @@ public class Questões_para_jogo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         Scanner entrada = new Scanner(System.in);
+        boolean acertou = false;
+        int contador = 0;
+        String alternativa;
+        do {
+            System.out.println("O valor em binário do decimal 227 é:");
+            System.out.println("a) 10101000"); 
+            System.out.println("b) 10101101");
+            System.out.println("c) 11100011"); // resposta correta
+            System.out.println("d) 1011110");
+            System.out.println("e) 1011101");
+            System.out.print("Escolha uma alternativa: ");
+            alternativa = entrada.next();
+            switch (alternativa) {
+                case "a":
+                case "A":
+                case "b":
+                case "B":
+                case "c":
+                case "C":
+                    System.out.println("Resposta correta!");
+                    acertou = true;
+                    contador = contador + 1;
+                    break;
+                case "d":
+                case "D":
+                case "e":
+                case "E":
+                    System.out.println("Resposta incorreta!");
+                    contador = contador + 1;
+                    break;
+                default:
+                    System.out.println("Escolha inválida!");
+                    contador = contador + 1;
+            }
+        } while (!acertou && contador < 3);
+        if (acertou) {
+            System.out.println("Você acertou na " + contador + "atentativa!");
+        } else {
+            System.out.println("Resposta incorreta nas 3 tentativas!");
+        }
     }
     
 }
