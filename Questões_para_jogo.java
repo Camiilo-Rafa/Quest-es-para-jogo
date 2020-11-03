03583425c23a9f1143914ea61da20ebb489e7084
Camiilo-Rafa <72233498+Camiilo-Rafa@users.noreply.github.com>
2020-11-03 12:54:44 -0300

    Questões do jogo

---
+++ Questões_para_jogo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questões_para_jogo;
+
+/**
+ *
+ * @author rafac
+ */
+public class Questões_para_jogo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
